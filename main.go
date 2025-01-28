44956ad7ce98647073fa23adbb47119b07ab75fa
mayuraviraj <mayuraviraj@gmail.com>
2025-01-28 22:40:15 +0100

    Added more logic.

--- main.go
+++ main.go
@@ -1,6 +1,9 @@
 package main
 
-import "fmt"
+import (
+	"fmt"
+	"strings"
+)
 
 func main() {
 	var conferenceName = "Go Conference"
@@ -10,25 +13,49 @@ func main() {
 	fmt.Printf("We have a total of %v tickets and remaining %v\n", conferenceTickets, remainingTickets)
 	fmt.Println("Get your tickets here to attend")
 
-	var firstName, lastName, email string
-	var userTickets uint
-	// ask the user for their name
-	fmt.Println("Enter your first name: ")
-	fmt.Scan(&firstName)
+	var bookings []string
 
-	fmt.Println("Enter your last name: ")
-	fmt.Scan(&lastName)
+	for {
+		var firstName, lastName, email string
+		var userTickets uint
+		// ask the user for their name
+		fmt.Println("Enter your first name: ")
+		fmt.Scan(&firstName)
 
-	fmt.Println("Enter your email: ")
-	fmt.Scan(&email)
+		fmt.Println("Enter your last name: ")
+		fmt.Scan(&lastName)
 
-	fmt.Println("Enter no of tickets: ")
-	fmt.Scan(&userTickets)
+		fmt.Println("Enter your email: ")
+		fmt.Scan(&email)
 
-	remainingTickets = remainingTickets - userTickets
+		fmt.Println("Enter no of tickets: ")
+		fmt.Scan(&userTickets)
 
-	fmt.Printf("Thank you %v %v for booking %v tickets. You will receive a email %v \n", firstName, lastName,
-		userTickets, email)
-	fmt.Printf("%v tickets remaining for %v\n", remainingTickets, conferenceName)
+		if userTickets <= remainingTickets {
+			remainingTickets = remainingTickets - userTickets
+			bookings = append(bookings, firstName+" "+lastName)
+
+			fmt.Printf("Thank you %v %v for booking %v tickets. You will receive a email %v \n", firstName, lastName,
+				userTickets, email)
+			fmt.Printf("%v tickets remaining for %v\n", remainingTickets, conferenceName)
+			fmt.Printf("All the bookings %v\n", bookings)
+
+			firstNames := []string{}
+			for _, booking := range bookings {
+				var names = strings.Fields(booking)
+				firstNames = append(firstNames, names[0])
+			}
+			fmt.Printf("The first names are: %v\n", firstNames)
+
+			if remainingTickets == 0 {
+				fmt.Println("Goodbye!")
+				break
+			}
+		} else {
+			fmt.Println("You have exceeded the number of tickets. Try again booking.")
+			continue
+		}
+
+	}
 
 }
